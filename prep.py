4cecb21862920c6500b0eaca236c8ef0adb3878f
shadmeoli <shadcodes@gmail.com>
2023-04-25 09:59:11 +0300

    INFO: Simple prep function

--- prep.py
+++ prep.py
@@ -7,4 +7,24 @@ def getPrimes(nums: list[int]) -> list[int]:
 
 rev = lambda vals: ''.join([vals[len(vals)-1-idx] for idx, i in enumerate(vals)])
 
-print(rev('king'))
+# find missing
+'''
+    arr1
+    arr2 - vals from arr1 but a missing val
+
+    input:  [2, 3, 4, 7, 9] | [2, 4, 7, 9]
+    output: 3
+'''
+
+def missing(arr1, arr2) -> int:
+    return [i for i in arr1 if i not in arr2][0]
+    # for idx, i in enumerate(arr1):
+
+    #     if i not in arr2:
+    #         return i
+    #         break
+    #     else:
+    #         continue
+
+
+print(missing([2, 3, 4, 7, 9], [2, 4, 7, 9]))
